94a38158ace4a503b302f0b877d613b123917945
SealCoding1337 <157487369+SealCoding1337@users.noreply.github.com>
2024-10-24 10:11:14 +0200

    Update whatDoesThis.cpp

--- WhatDoesThisCode/whatDoesThis.cpp
+++ WhatDoesThisCode/whatDoesThis.cpp
@@ -160,8 +160,6 @@ void qWhatDoesThis::Render( const q3Transform& tx, bool awake, q3Render* render 
 		q3Vec3(  e.x,  e.y,  e.z )
 	};
 
-    //this is a change
-
 	for ( i32 i = 0; i < 36; i += 3 )
 	{
 		q3Vec3 a = q3Mul( world, vertices[ kBoxIndices[ i ] ] );
